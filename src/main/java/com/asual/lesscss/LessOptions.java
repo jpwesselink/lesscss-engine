a83c9e3278b374542b3556a5eeb1bc1e131f7a21
JP Wesselink <jpwesselink@gmail.com>
2014-01-28 10:18:44 +0100

    Prepare lessjs 1.5.1

--- src/main/java/com/asual/lesscss/LessOptions.java
+++ src/main/java/com/asual/lesscss/LessOptions.java
@@ -34,7 +34,7 @@ public class LessOptions {
 	private String charset = "UTF-8";
 	private Boolean compress = false;
 	private Boolean css = false;
-	private URL less = getClass().getClassLoader().getResource("META-INF/1.5.0/less.js");
+	private URL less = getClass().getClassLoader().getResource("META-INF/1.5.1/less.js");
 	private String lineNumbers;
 	private Integer optimization = 3;
 
